82e9e886b9f132b281c36e0b376dcfe3e2b408f2
MajesticBeast <majestic@thelostsons.net>
2024-02-17 15:41:57 -0800

    updates

--- main.tf
+++ main.tf
@@ -6,15 +6,9 @@
 # Created: 2019-08-01
 # Description: This module creates an S3 bucket in AWS
 
-resource "random_pet" "bucket_suffix" {
-  length = 5
-  separator = "-"
-}
 
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "a-unique-bucket-name-${random_pet.bucket_suffix.id}"
+  bucket = var.bucket
 
-  tags = {
-    Name = "My bucket"
-  }
+  tags = var.tags
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,12 @@
 module "tags" {
-  source = "app.terraform.io/thelostsons/modules/aws/tags"
+  source = "app.terraform.io/thelostsons/tags/aws"
+  version = "0.0.1"
+}
+
+variable "tags" {
+  type = map(string)
+}
+
+variable "bucket" {
+  type = string
 }
